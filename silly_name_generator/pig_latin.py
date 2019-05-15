55c645a7803fa7bd75640ca893d338794eda3424
Conor O'Mara <omarac@tcd.ie>
2019-05-15 22:28:52 +0100

    Pig latin file

--- silly_name_generator/pig_latin.py
+++ silly_name_generator/pig_latin.py
@@ -9,17 +9,51 @@ Pig Latin
 
 import sys
 
-def main(args):
+def pig_latin(word):
+    vowels = 'aeiou'
+    consonants = 'bcdfghjklmnpqrstvwxzy'
+    
+    if ((len(word) > 1) & (word[0] != 'y') & ("y" in word[1:].lower()) & (word[1:] not in consonants) & (word[0] not in vowels)):
+        y_index = [ i for i in range(len(word)) if word[i].lower() == 'y'] 
+        first_vowel = [ i for i in range(len(word)) if word[i].lower() not in consonants]
+        if (y_index[0] < first_vowel[0]):
+            return pig_y(word.lower(),y_index[0])
+        else:
+            return pig_cons(word.lower(),first_vowel[0])
+    elif ((len(word) > 1) & (word[0] != 'y') & ("y" in word[1:].lower()) & (word[0] not in vowels)):
+        y_index = [ i for i in range(len(word)) if word[i].lower() == 'y']
+        return pig_y(word.lower(),y_index[0])
+    else:
+        first_vowel = [ i for i in range(len(word)) if word[i].lower() not in consonants] 
+        if (first_vowel[0] == 0):
+            return pig_vow(word.lower())
+        elif (first_vowel[0] > 0):
+            return pig_cons(word,first_vowel[0])
+                
+def pig_y(word,i):
+    return word[i:]+"-"+word[0:i]+"ay"
+
+def pig_cons(word,i):
+    return word[i:]+"-"+word[0:i]+"ay"
+
+def pig_vow(word):
+    return word+"-"+"yay"
+
+def main():
+    """ Pseudocode
+    
+    """
     print("This code will write a phrase of your choice in Pig Latin\n")
-    user_phrase = input("Please enter your phrase")
+    user_phrase = raw_input("Please enter your phrase: ")
 
     phrase_list = user_phrase.split()
-    
-    dict_vowel = {'vowels':['a','e','i','o','u']}
-    
-    for i in len(phrase_list):
-        
+    output = []
+
+    [output.append(pig_latin(phrase_list[i])) for i in range(len(phrase_list))]
+    final_phrase = " ".join(output)
+    print(final_phrase)
+
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main()
